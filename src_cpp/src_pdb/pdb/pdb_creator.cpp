15de89b30798e9e7a76159a3b53a8564a536b357
Trass3r <trass3r@gmail.com>
2026-09-17 04:16:36 +0200

    revert pdb guid generation to fix DBI age

--- src_cpp/src_pdb/pdb/pdb_creator.cpp
+++ src_cpp/src_pdb/pdb/pdb_creator.cpp
@@ -64,7 +64,7 @@ namespace FakePDB::PDB {
         // Add an Info stream.
         auto& InfoBuilder = _pdbBuilder.getInfoBuilder();
         InfoBuilder.setVersion(llvm::pdb::PdbRaw_ImplVer::PdbImplVC70);
-        InfoBuilder.setHashPDBContentsToGUID(true);
+        InfoBuilder.setHashPDBContentsToGUID(false);
 
         // set GUID and Aage
         if (std::filesystem::exists(path_exe)) {
